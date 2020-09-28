c1fdfb7d0ffa1e1754e87ebb6a39bb1148139f0c
Diego Lesmes <ing.dlesmes@gmail.com>
2020-09-28 02:39:10 -0500

    Code final, with out Assessment2

--- ML/ML_Regularization.R
+++ ML/ML_Regularization.R
@@ -216,3 +216,316 @@ schools %>% group_by(id) %>%
 
 # Regula...
 overall <- mean(sapply(scores, mean))
+alpha <- 25
+regularized <- sapply(1:nrow(schools), function(i){
+  sum(scores[[i]] - overall)/(alpha+schools$size[i])
+})
+schools %>% mutate(Regularization = overall + sapply(regularized, mean)) %>% 
+  arrange(desc(Regularization)) %>% 
+  top_n(10,Regularization)
+#Tunning alpha
+alpha <- seq(10,250)
+
+RMSEXAlpha <- sapply(alpha, function(j){
+  regularized <- sapply(1:nrow(schools), function(i){
+    sum(scores[[i]] - overall)/(j+schools$size[i])
+  })
+  schools_Reg <- schools %>% mutate(Regularization = overall + sapply(regularized, mean))
+  RMSE(schools_Reg$quality,schools_Reg$Regularization)
+})
+plot(alpha, RMSEXAlpha, higlight = TRUE)
+best_alpha <- alpha[which.min(RMSEXAlpha)]
+best_alpha
+#Best Regula
+alpha <- best_alpha
+regularized <- sapply(1:nrow(schools), function(i){
+  sum(scores[[i]] - overall)/(alpha+schools$size[i])
+})
+schools %>% mutate(Regularization = overall + sapply(regularized, mean)) %>% 
+  arrange(desc(Regularization)) %>% 
+  top_n(10,Regularization)
+
+#Common mistake
+alpha <- seq(10,250)
+
+RMSEXAlpha <- sapply(alpha, function(j){
+  regularized <- sapply(1:nrow(schools), function(i){
+    sum(scores[[i]])/(j+schools$size[i])
+  })
+  schools_Reg <- schools %>% mutate(Regularization = sapply(regularized, mean))
+  RMSE(schools_Reg$quality,schools_Reg$Regularization)
+})
+plot(alpha, RMSEXAlpha)
+best_alpha <- alpha[which.min(RMSEXAlpha)]
+best_alpha
+
+### Matrix Factorization____________________________________________________________________ ----
+train_small <- movielens %>% 
+  group_by(movieId) %>%
+  filter(n() >= 50 | movieId == 3252) %>% ungroup() %>% #3252 is Scent of a Woman used in example
+  group_by(userId) %>%
+  filter(n() >= 50) %>% ungroup()
+
+y <- train_small %>% 
+  select(userId, movieId, rating) %>%
+  spread(movieId, rating) %>%
+  as.matrix()
+
+rownames(y)<- y[,1]
+y <- y[,-1]
+colnames(y) <- with(movie_titles, title[match(colnames(y), movieId)])
+
+y <- sweep(y, 1, rowMeans(y, na.rm=TRUE))
+y <- sweep(y, 2, colMeans(y, na.rm=TRUE))
+
+m_1 <- "Godfather, The"
+m_2 <- "Godfather: Part II, The"
+qplot(y[ ,m_1], y[,m_2], xlab = m_1, ylab = m_2)
+
+m_1 <- "Godfather, The"
+m_3 <- "Goodfellas"
+qplot(y[ ,m_1], y[,m_3], xlab = m_1, ylab = m_3)
+
+m_4 <- "You've Got Mail" 
+m_5 <- "Sleepless in Seattle" 
+qplot(y[ ,m_4], y[,m_5], xlab = m_4, ylab = m_5)
+
+cor(y[, c(m_1, m_2, m_3, m_4, m_5)], use="pairwise.complete") %>% 
+  knitr::kable()
+
+set.seed(1)
+options(digits = 2)
+Q <- matrix(c(1 , 1, 1, -1, -1), ncol=1)
+rownames(Q) <- c(m_1, m_2, m_3, m_4, m_5)
+P <- matrix(rep(c(2,0,-2), c(3,5,4)), ncol=1)
+rownames(P) <- 1:nrow(P)
+
+X <- jitter(P%*%t(Q))
+X %>% knitr::kable(align = "c")
+
+cor(X)
+
+t(Q) %>% knitr::kable(aling="c")
+
+P
+
+set.seed(1)
+options(digits = 2)
+m_6 <- "Scent of a Woman"
+Q <- cbind(c(1 , 1, 1, -1, -1, -1), 
+           c(1 , 1, -1, -1, -1, 1))
+rownames(Q) <- c(m_1, m_2, m_3, m_4, m_5, m_6)
+P <- cbind(rep(c(2,0,-2), c(3,5,4)), 
+           c(-1,1,1,0,0,1,1,1,0,-1,-1,-1))/2
+rownames(P) <- 1:nrow(X)
+
+X <- jitter(P%*%t(Q), factor=1)
+X %>% knitr::kable(align = "c")
+
+cor(X)
+
+t(Q) %>% knitr::kable(align="c")
+
+P
+
+six_movies <- c(m_1, m_2, m_3, m_4, m_5, m_6)
+tmp <- y[,six_movies]
+cor(tmp, use="pairwise.complete")
+
+
+y[is.na(y)] <- 0
+y <- sweep(y, 1, rowMeans(y))
+pca <- prcomp(y)
+
+dim(pca$rotation)
+
+dim(pca$x)
+
+plot(pca$sdev)
+
+var_explained <- cumsum(pca$sdev^2/sum(pca$sdev^2))
+plot(var_explained)
+
+### principal component analysis (PCA) or singular value decomposition (SVD)___________ ----
+y[is.na(y)] <- 0
+y <- sweep(y, 1, rowMeans(y))
+pca <- prcomp(y)
+dim(pca$rotation)
+
+dim(pca$x)
+
+plot(pca$sdev)
+
+var_explained <- cumsum(pca$sdev^2/sum(pca$sdev^2))
+plot(var_explained)
+
+library(ggrepel)
+pcs <- data.frame(pca$rotation, name = colnames(y))
+pcs %>%  ggplot(aes(PC1, PC2)) + geom_point() + 
+  geom_text_repel(aes(PC1, PC2, label=name),
+                  data = filter(pcs, 
+                                PC1 < -0.1 | PC1 > 0.1 | PC2 < -0.075 | PC2 > 0.1))
+
+pcs %>% select(name, PC1) %>% arrange(PC1) %>% slice(1:10)
+
+pcs %>% select(name, PC1) %>% arrange(desc(PC1)) %>% slice(1:10)
+
+pcs %>% select(name, PC2) %>% arrange(PC2) %>% slice(1:10)
+
+pcs %>% select(name, PC2) %>% arrange(desc(PC2)) %>% slice(1:10)
+
+###Comprehension Check 2_________________________________________________________________----
+#set.seed(1987)
+#if using R 3.6 or later, use `
+set.seed(1987, sample.kind="Rounding")#` instead
+n <- 100
+k <- 8
+Sigma <- 64  * matrix(c(1, .75, .5, .75, 1, .5, .5, .5, 1), 3, 3) 
+m <- MASS::mvrnorm(n, rep(0, 3), Sigma)
+m <- m[order(rowMeans(m), decreasing = TRUE),]
+y <- m %x% matrix(rep(1, k), nrow = 1) + matrix(rnorm(matrix(n*k*3)), n, k*3)
+colnames(y) <- c(paste(rep("Math",k), 1:k, sep="_"),
+                 paste(rep("Science",k), 1:k, sep="_"),
+                 paste(rep("Arts",k), 1:k, sep="_"))
+
+my_image <- function(x, zlim = range(x), ...){
+  colors = rev(RColorBrewer::brewer.pal(9, "RdBu"))
+  cols <- 1:ncol(x)
+  rows <- 1:nrow(x)
+  image(cols, rows, t(x[rev(rows),,drop=FALSE]), xaxt = "n", yaxt = "n",
+        xlab="", ylab="",  col = colors, zlim = zlim, ...)
+  abline(h=rows + 0.5, v = cols + 0.5)
+  axis(side = 1, cols, colnames(x), las = 2)
+}
+
+my_image(y)
+
+my_image(cor(y), zlim = c(-1,1))
+range(cor(y))
+axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
+
+#SVD
+s <- svd(y)
+names(s)
+y_svd <- s$u %*% diag(s$d) %*% t(s$v)
+max(abs(y - y_svd))
+
+
+ss_y <- colSums(y^2)
+sum(ss_y)
+
+ss_yv <- colSums((s$u %*% diag(s$d))^2)
+sum(ss_yv)
+
+sum(ss_y) == sum(ss_yv)
+
+plot(ss_y)
+plot(ss_yv)
+
+plot(sqrt(ss_yv),s$d)
+
+yv <- y%*%s$v
+pca <- prcomp(yv)
+pcs_sum <- summary(pca)
+pcs_sum
+sum(pcs_sum$importance[2,1:3])
+
+##Use the sweep function to compute  UD  without constructing diag(s$d) or using matrix multiplication
+identical(s$u %*% diag(s$d), sweep(s$u, 2, s$d, FUN = "*"))
+
+ud <- sweep(s$u, 2, s$d, FUN = "*")
+Arv_Student <- rowMeans(y)
+plot(Arv_Student,ud[,1])
+
+my_image(s$v)
+my_image(s$u)
+my_image(ud)
+
+str(s$u)
+str(s$v)
+str(ud)
+
+my_image(ud[,1]%*%t(s$v[,1]))
+my_image(ud[,2]%*%t(s$v[,2]))
+my_image(ud[,3]%*%t(s$v[,3]))
+
+plot(s$u[,1], ylim = c(-0.25, 0.25))
+plot(s$v[,1], ylim = c(-0.25, 0.25))
+with(s, my_image((u[, 1, drop=FALSE]*d[1]) %*% t(v[, 1, drop=FALSE])))
+my_image(y)
+
+resid <- y - with(s,(u[, 1, drop=FALSE]*d[1]) %*% t(v[, 1, drop=FALSE]))
+my_image(cor(resid), zlim = c(-1,1))
+axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
+
+plot(s$u[,2], ylim = c(-0.25, 0.25))
+plot(s$v[,2], ylim = c(-0.25, 0.25))
+with(s, my_image((u[, 2, drop=FALSE]*d[1]) %*% t(v[, 2, drop=FALSE])))
+my_image(y)
+
+resid <- y - with(s,sweep(u[, 1:2], 2, d[1:2], FUN="*") %*% t(v[, 1:2]))
+my_image(cor(resid), zlim = c(-1,1))
+axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
+
+plot(s$u[,3], ylim = c(-0.25, 0.25))
+plot(s$v[,3], ylim = c(-0.25, 0.25))
+with(s, my_image((u[, 3, drop=FALSE]*d[1]) %*% t(v[, 3, drop=FALSE])))
+my_image(y)
+
+resid <- y - with(s,sweep(u[, 1:3], 2, d[1:3], FUN="*") %*% t(v[, 1:3]))
+my_image(cor(resid), zlim = c(-1,1))
+axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
+
+y_hat <- with(s,sweep(u[, 1:3], 2, d[1:3], FUN="*") %*% t(v[, 1:3]))
+my_image(y, zlim = range(y))
+my_image(y_hat, zlim = range(y))
+my_image(y - y_hat, zlim = range(y))
+
+
+##Comprehension Check: Dimension Reduction----
+data("tissue_gene_expression")
+dim(tissue_gene_expression$x)
+tiss_X <-tissue_gene_expression$x
+tiss_y <-tissue_gene_expression$y
+
+x <- sweep(tiss_X, 1, rowMeans(tissue_gene_expression$x))
+pc <- prcomp(x)
+data.frame(pc_1 = pc$x[,1], pc_2 = pc$x[,2], 
+           tissue = tissue_gene_expression$y) %>%
+  ggplot(aes(pc_1, pc_2, color = tissue)) +
+  geom_point()
+
+plot(rowMeans(tissue_gene_expression$x),pc$x[,1])
+cor(rowMeans(tissue_gene_expression$x),pc$x[,1])
+
+x <- with(tissue_gene_expression, sweep(x, 1, rowMeans(x)))
+x <- with(tissue_gene_expression, sweep(x, 1, mean(x)))
+pc <- prcomp(x)
+data.frame(pc_1 = pc$x[,1], pc_2 = pc$x[,2], 
+           tissue = tissue_gene_expression$y) %>%
+  ggplot(aes(pc_1, pc_2, color = tissue)) +
+  geom_point()
+
+plot_PC1 <- bind_cols(tiss_y,pc$x[,7])
+names(plot_PC1) <- c("Tissues","PC7")
+plot_PC1 %>% ggplot(aes(Tissues,PC7))+
+  geom_boxplot()
+
+
+pc_summary <- summary(pc)
+sum(pc_summary$importance[2,1:3])
+
+##Clustering______________________________________----
+
+d <- dist(tissue_gene_expression$x - rowMeans(tissue_gene_expression$x))
+h <- hclust(d)
+plot(h, cex = 0.65, main = "", xlab = "")
+
+library(RColorBrewer)
+sds <- matrixStats::colSds(tissue_gene_expression$x)
+ind <- order(sds, decreasing = TRUE)[1:50]
+colors <- brewer.pal(7, "Dark2")[as.numeric(tissue_gene_expression$y)]
+heatmap(t(tissue_gene_expression$x[,ind]),
+        col = brewer.pal(11, "RdBu"),
+        scale = "row",
+        ColSideColors = colors)
